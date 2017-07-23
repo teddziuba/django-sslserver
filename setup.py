1542d3d5d5446859ed69f58146b78347fe7d0afe
johnthagen <johnthagen@gmail.com>
2017-07-23 13:47:31 -0400

    Add trove classifiers

--- setup.py
+++ setup.py
@@ -16,5 +16,22 @@ setup(name="django-sslserver",
                                   "certs/server.csr"]},
       install_requires=["setuptools",
                         "Django >= 1.8"],
-      license="MIT"
+      license="MIT",
+
+      classifiers=[
+          "Environment :: Web Environment",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Framework :: Django",
+          "Framework :: Django :: 1.8",
+          "Framework :: Django :: 1.9",
+          "Framework :: Django :: 1.10",
+          "Framework :: Django :: 1.11"],
       )
